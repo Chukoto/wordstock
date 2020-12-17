3d09f693cfd4cb3b7a0b6ab6e8eafc7267e53030
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-17 14:01:44 +0900

    Partモデルの作成

---
+++ app/models/part.rb
@@ -0,0 +1,13 @@
+class Part < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: 'Noun（名詞）' },
+    { id: 2, name: 'Pronoun（代名詞）' },
+    { id: 3, name: 'Verb（動詞）' }
+    { id: 4, name: 'Adjective（形容詞）' }
+    { id: 5, name: 'Adverb（副詞）' }
+    { id: 6, name: 'Preposition（前置詞）' }
+    { id: 7, name: 'Conjunction（接続詞）' }
+    { id: 8, name: 'Interjection（間投詞）' }
+  ]
+end
