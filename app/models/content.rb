c666c6714584bbd479bad7e202616e637736b2eb
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-17 14:02:08 +0900

    ActiveHash追加に伴うcontent記述の修正

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,6 @@
 class Content < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :part
 
   with_options presence: true do
     validates :part_id, null: false
