6c50a07c3a89d3456d4e4dbdea5142c953e6ac15
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-17 13:46:20 +0900

    migrationファイルにカラム追加

--- db/migrate/20201217043827_create_contents.rb
+++ db/migrate/20201217043827_create_contents.rb
@@ -1,7 +1,8 @@
 class CreateContents < ActiveRecord::Migration[6.0]
   def change
     create_table :contents do |t|
-
+      t.integer :part_id, null: false
+      t.string  :word, null: false
       t.timestamps
     end
   end
