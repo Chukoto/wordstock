c0aca6311af7c0a239de5afa2c766b532fdb836c
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-25 14:47:16 +0900

    descriptionsコントローラーの記述（new, createアクションとparams）

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,2 +1,20 @@
 class DescriptionsController < ApplicationController
+  def new
+    @description = Description.new
+  end
+
+  def create
+    @description = Description.new(description_params)
+    if @description.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def description_params
+    params.require(:description).permit(:english_text).merge(content_id: params[:content_id], user_id: current_user.id)
+  end
 end
