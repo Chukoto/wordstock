fe5954ad6164ca538d6654e1c641e30b83d5b15a
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-26 23:11:37 +0900

    アソシエーション修正（userとcontentsを紐付けたため）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   has_many :descriptions      
+  has_many :contents
+  has_many :descriptions
 end
