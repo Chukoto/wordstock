30348d8f2527a7dbc1524431ca841b05d51ffc9b
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-28 15:30:36 +0900

    migration記述（english_text, japanese_textカラムを追加）

--- db/migrate/20201228025655_create_sentences.rb
+++ db/migrate/20201228025655_create_sentences.rb
@@ -1,7 +1,10 @@
 class CreateSentences < ActiveRecord::Migration[6.0]
   def change
     create_table :sentences do |t|
-
+      t.text :english_text, null: false
+      t.text :japanese_text, null: true
+      t.references :user, null: false, foreign_key: true
+      t.references :content, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_041555) do
+ActiveRecord::Schema.define(version: 2020_12_28_025655) do
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "part_id", null: false
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 2020_12_24_041555) do
     t.index ["user_id"], name: "index_descriptions_on_user_id"
   end
 
+  create_table "sentences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "english_text", null: false
+    t.text "japanese_text", null: false
+    t.bigint "user_id", null: false
+    t.bigint "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_sentences_on_content_id"
+    t.index ["user_id"], name: "index_sentences_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +58,6 @@ ActiveRecord::Schema.define(version: 2020_12_24_041555) do
   add_foreign_key "contents", "users"
   add_foreign_key "descriptions", "contents"
   add_foreign_key "descriptions", "users"
+  add_foreign_key "sentences", "contents"
+  add_foreign_key "sentences", "users"
 end
