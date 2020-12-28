60f84d3166481ff9e5581ca839e686afac7d5a20
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-28 15:28:02 +0900

    sentenceモデルにバリデーション追加(english_text, japanese_text)

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,8 @@
 class Sentence < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :content
+
+  validates :english_text, presence: true, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :japanese_text, presence: false, format: { without: /\A[a-zA-Z\s]+\z/ }
 end
