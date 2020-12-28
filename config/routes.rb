caecbf9ebf196737e756c73fb9c719dbe6efe98e
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-28 22:35:38 +0900

    contentsにルーティング追加（edit, update）

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'contents#index'
 
-  resources :contents, only: [:index, :new, :create, :show] do
+  resources :contents, only: [:index, :new, :create, :show, :edit, :update] do
     resources :descriptions, only: [:new, :create]
     resources :sentences, only: [:new, :create]
   end
