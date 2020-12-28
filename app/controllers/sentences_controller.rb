9907842c1f6bcdcab5493c6987971c8e03877e54
Haruki Hirai <iamprivateengineer@gmail.com>
2020-12-28 11:55:10 +0900

    sentencesコントローラーの記述（new, createアクション、sentence_paramsを追記）

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,2 +1,20 @@
 class SentencesController < ApplicationController
+  def new
+    @sentence = Sentence.new
+  end
+
+  def create
+    @sentence = Sentence.new(sentence_params)
+    if @sentence.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sentence_params
+    params.require(:sentence).permit(:english_text, :japanese_text).merge(content_id: params[:content_id], user_id: current_user.id)
+  end
 end
