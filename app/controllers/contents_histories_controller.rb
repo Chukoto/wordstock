e8efcf590148b23045cefc6f4c1f51934489345f
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-02 20:57:47 +0900

    contents_historiesコントローラ編集（showアクションの追加）

--- app/controllers/contents_histories_controller.rb
+++ app/controllers/contents_histories_controller.rb
@@ -1,2 +1,8 @@
 class ContentsHistoriesController < ApplicationController
+
+  def show
+    @contents_history = ContentsHistory.find(params[:id])
+    @content_word = @contents_history.content.word
+    @contents_histories = ContentsHistory.where(content_id: params[:content_id]).order('created_at DESC')
+  end
 end
