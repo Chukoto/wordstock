68dd83cd283e31fdcd4b78d2ffbdd1337f2d88df
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-02 20:55:01 +0900

    contents_historiesテーブル編集（カラムwordの追加）

--- db/migrate/20210101142131_create_contents_histories.rb
+++ db/migrate/20210101142131_create_contents_histories.rb
@@ -1,6 +1,7 @@
 class CreateContentsHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :contents_histories do |t|
+      t.string :word, null: false
       t.references :user, null: false, foreign_key: true
       t.references :content, null: false, foreign_key: true
       t.timestamps
