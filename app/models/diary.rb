226e63152fb974fc1363fab1905730627d1ce38f
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-04 23:06:52 +0900

    diaryモデルの編集（日記の項目未記入での保存不可にする記述）

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,9 @@
 class Diary < ApplicationRecord
   belongs_to :user
 
-  validates :title, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :text, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  with_options presence: true do
+    validates :title, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :text, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  end
+  
 end
