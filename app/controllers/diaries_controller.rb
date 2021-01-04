4e89a678ef2647aff1fe5035c6eb898d927e4326
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-04 23:05:43 +0900

    diariesコントローラに追記（new, createアクションの追加）

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,23 @@
 class DiariesController < ApplicationController
   def show
   end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diary_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :text).merge(user_id: current_user.id)
+  end
 end
