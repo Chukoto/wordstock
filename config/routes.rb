18b331193d82741de2bc9a86ea5dd66857627832
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-07 13:13:04 +0900

    routes.rbの編集（不要になったためpasswordsのパスを削除）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
   }
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
