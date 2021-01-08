1a1b36cdf971e6bd2f5306796e16b735e7185f7c
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-08 16:51:34 +0900

    profilesコントローラの記述（show, new, createアクションの追加）

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,21 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @profile = Profile.find_by(user_id: params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:text).merge(user_id: current_user.id)
+  end
+
 end
