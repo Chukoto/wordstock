89e9655ec209eb3ff9f1ad1695e643c71154bbe9
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-08 16:00:25 +0900

    profilesのmigrationファイルに追記（textカラム、外部キーuserの追加）

--- db/migrate/20210108065000_create_profiles.rb
+++ db/migrate/20210108065000_create_profiles.rb
@@ -1,7 +1,8 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.text :text, null: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
