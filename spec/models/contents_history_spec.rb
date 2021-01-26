e2524ea4eb34a9aeb516859a440f90f3f4f9bc29
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-26 12:33:18 +0900

    contents_history.rbの修正（意味を単語に変更）

--- spec/models/contents_history_spec.rb
+++ spec/models/contents_history_spec.rb
@@ -4,8 +4,8 @@ describe ContentsHistory do
     @contents_history = FactoryBot.build(:contents_history)
   end
 
-  describe '意味編集履歴の作成' do
-    context '意味編集履歴の作成がうまくいく時' do
+  describe '単語編集履歴の作成' do
+    context '単語編集履歴の作成がうまくいく時' do
       it "user_id, content_id, wordが存在すれば保存されること" do
         expect(@contents_history).to be_valid
       end
