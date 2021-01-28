0370271588e360fed9eaeeb51a8cd8e25a3d7447
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-28 15:53:57 +0900

    sentence.rbの修正（正規表現の修正）

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,6 +2,6 @@ class Sentence < ApplicationRecord
   belongs_to :user
   belongs_to :content
 
-  validates :english_text, presence: true, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :japanese_text, presence: false, format: { without: /\A[a-zA-Z\s]+\z/ }
+  validates :english_text, presence: true, format: { with: /\A[\s\w!?,."'¥$%&]+\z/ }
+  validates :japanese_text, presence: false, format: { with: /\A[\s\p{blank}ぁ-んァ-ン一-龥。、・「」！？]+\z/ }
 end
