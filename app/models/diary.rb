ead3e310785fdc5ebe0fd43f7bc2cf736b6b8b8f
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-29 21:32:29 +0900

    diary.rbの修正（正規表現の変更・リファクタリング）

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,7 +2,7 @@ class Diary < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    validates :title, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :text, format: { without: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :title, format: { with: /\A[\w\s!?,."'¥$%&]+\z/ }
+    validates :text, format: { with: /\A[\w\s!?,."'¥$%&]+\z/ }
   end
 end
