1c520a25131bed2d55bdca2d8b4e8870be609408
Haruki Hirai <iamprivateengineer@gmail.com>
2021-01-30 21:57:02 +0900

    sentence.rbの修正（japanese_textから、不要正規表現の記述を削除）

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,5 +3,5 @@ class Sentence < ApplicationRecord
   belongs_to :content
 
   validates :english_text, presence: true, format: { with: /\A[\s\w!?,."'¥$%&]+\z/ }
-  validates :japanese_text, presence: false, format: { with: /\A[\s\p{blank}ぁ-んァ-ン一-龥。、・「」！？]+\z/ }
+  validates :japanese_text, presence: false
 end
